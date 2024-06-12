da4176c125622dab81d0490b2ebc530d032354d8
RullyBagjaaa <reallybagjaa@gmail.com>
2024-06-12 19:21:48 +0700

    volunteerr

--- resources/views/shelter.blade.php
+++ resources/views/shelter.blade.php
@@ -19,13 +19,13 @@
     <div class="row">
         <div class="col-12 d-flex align-items-center mt-3 mb-5">
             <!-- Tombol Back di Kiri -->
-            <button onclick="history.back()" class="btn btn-light">
+            <a href="/shelter"><button onclick="history.back()" class="btn btn-light">
                 <i class="fas fa-arrow-left fa-xl"></i>
-            </button>
+            </button></a>
             <!-- Breadcrumb di Kanan -->
             <nav aria-label="breadcrumb" class="ms-auto">
                 <ol class="breadcrumb mb-0">
-                    <li class="breadcrumb-item"><a href="#" class="text-decoration-none">Shelter</a></li>
+                    <li class="breadcrumb-item"><a href="/shelter" class="text-decoration-none">Shelter</a></li>
                     <li class="breadcrumb-item active" aria-current="page">{{ $shelter->nama_shelter }}</li>
                 </ol>
             </nav>
@@ -33,7 +33,7 @@
     </div>
     <!-- Hero -->
     <div class="justify-content-start">
-        <div class="p-5 shadow-4 bg-accent-500">
+        <div class="p-5 shadow-4 bg-accent-500 rounded-4">
             <div class="d-flex">
                 <div>
                     <img src="{{ asset('uploads/' . $shelter->foto) }}" alt="Foto Shelter" class="img-thumbnail img-fluid" style="width: 200px; height: 200px; object-fit: cover;">
@@ -46,7 +46,7 @@
                     </div>
                     <div class="d-flex align-items-center mb-3">
                         <i class="fa-solid fa-calendar-days fa-lg me-2"></i>
-                        <p class="mb-0">Senin - Sabtu: 09.00 - 19.00<br>Minggu: Libur</p>
+                        <p class="mb-0">{{ $shelter->hari_buka }}: {{ $shelter->jam_buka }}</p>
                     </div>
                     <div class="d-flex align-items-center mb-3">
                         <i class="fa-solid fa-phone-volume fa-lg me-2"></i>
@@ -75,24 +75,44 @@
     {{-- section3 --}}
     <div class="row my-5">
         <div class="col-md-2">
-            <h5 class="text-white bg-accent-500 p-3 rounded-end">Shelter lainnya</h5>
+            <h5 class="text-white bg-accent-500 p-3 rounded-end-4">Shelter lainnya</h5>
         </div>
     </div>
 
     <div class="row row-cols-1 row-cols-md-4 g-4">
-        @foreach($shelters as $otherShelter)
+        {{-- @foreach($shelters as $otherShelter)
         <div class="col">
             <div class="card h-100 rounded-4">
                 <img src="{{ asset('uploads/' . $otherShelter->foto) }}" class="card-img-top img-fluid rounded-top-4 h-100" style="object-fit: cover;">
                 <div class="card-body">
                     <h5 class="card-title fw-bold">{{ $otherShelter->nama_shelter }}</h5>
-                    <h6 class="card-title">{{ $otherShelter->kota }}</h6>
-                    <p class="card-text">{{ $otherShelter->alamat_jalan }}</p>
+                    <h6 class="card-title">{{ $otherShelter->alamat_jalan }}, {{ $otherShelter->kota }}</h6>
+                    <p class="card-text">{{ $otherShelter->hari_buka }}: {{ $otherShelter->jam_buka }}</p>
                     <a href="{{ route('shelter.show', $otherShelter->id) }}" class="btn btn-accent text-white">Lihat Shelter</a>
                 </div>
             </div>
         </div>
-        @endforeach
+        @endforeach --}}
+        @foreach ($shelters as $otherShelter)
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3 py-4 d-flex justify-content-center card-container">
+            <div class="card shadow-shelter rounded-4 card-shelter text-sky-900" style="max-width: 18rem; padding:15px;">
+                <img src="{{ asset('uploads/' . $otherShelter->foto) }}" class="card-img-top rounded" alt="..."
+                    style="height: 200px; object-fit: cover; image-rendering: auto;">
+                <div class="card-body">
+                    <h5 class="card-title fw-bold">{{ $otherShelter->nama_shelter }}</h5>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item"><i class="fa-solid fa-location-dot"
+                            style="margin-right: 5px;"></i>{{ $otherShelter->alamat_jalan }}, {{ $otherShelter->kota }}</li>
+                    <li class="list-group-item"><i class="fa-solid fa-road "
+                            style="margin-right: 5px;"></i>{{ $otherShelter->hari_buka }}: {{ $otherShelter->jam_buka }}</li>
+                </ul>
+                <div class="d-flex justify-content-start mt-2 ps-2 py-2">
+                    <a href="{{ route('shelter.show', $otherShelter->id) }}" class="btn btn-secondary">Shelter Page</a>
+                </div>
+            </div>
+        </div>
+    @endforeach
     </div>
 </div>
 
--- resources/views/volunteer/dashboard.blade.php
+++ resources/views/volunteer/dashboard.blade.php
@@ -1,19 +1,132 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Pet and Shelter</title>
     <link href="https://fonts.googleapis.com/css2?family=Helvetica&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,1,0" />
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,1,0" />
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <link rel="stylesheet" href="../css/styleDashboard.css">
     <link rel="stylesheet" href="css/styleDashboard.css">
     <link href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
+
 <body>
-@include('volunteer.sidebar')
+    <style>
+        .flex-container {
+            display: flex;
+        }
+        
+        .sidebar {
+            flex: 0.1;
+        }
+        
+        .container {
+            flex: 3.5;
+            display: flex;
+            flex-direction: column;
+    align-items: flex-start;
+        }
+        .label {
+        margin-right: 10px;
+    }
+        
+        </style>
+
+    <div class="flex-container">
+        <div class="sidebar" style="z-index: 1000;">
+            @include('volunteer.sidebar', ['class' => 'sidebar'])
+        </div>
+        
+
+        <div class="container ">
+            <h1 style="margin-top: 10%; margin-bottom: 1%;">Dashboard Volunteer</h1>
+            @foreach ($rescues as $rescue)
+                <div class="card col-12">
+                    <h5 class="card-header">Rescue ID  {{ $rescue->id }}</h5>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <p class="card-text lh-lg "><strong class="label">Nama Pelapor : </strong>{{ $rescue->namaPelapor }}</p>
+                                <p class="card-text lh-lg"><strong class="label">Usia Pelapor : </strong>{{ $rescue->usiaPelapor }}</p>
+                            </div>
+                            <div class="col-md-3">
+                                <p class="card-text lh-lg"><strong class="label">Nomor Telepon: </strong>{{ $rescue->nomorTelp }}</p>
+                                <p class="card-text lh-lg"><strong class="label">Jenis Kelamin: </strong>{{ $rescue->jenisKelamin }}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="justify-content-start">
+                        <div class="d-flex p-5">
+                            <div style="width: 300px; overflow: hidden;">
+                                <div id="carouselId{{ $rescue->id }}" class="carousel slide" data-bs-ride="carousel">
+                                    <div class="carousel-inner">
+                                        <div class="carousel-item active">
+                                            <img src="{{ asset('uploads/' . $rescue->fotoHewan) }}" class="d-block w-100">
+                                        </div>
+                                        <div class="carousel-item">
+                                            <img src="{{ asset('uploads/' . $rescue->fotoLokasi) }}" class="d-block w-100">
+                                        </div>
+                                    </div>
+                                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselId{{ $rescue->id }}" data-bs-slide="prev">
+                                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Previous</span>
+                                    </button>
+                                    <button class="carousel-control-next" type="button" data-bs-target="#carouselId{{ $rescue->id }}" data-bs-slide="next">
+                                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Next</span>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="ms-5 ">
+                                <h2 class="fw-bold mb-4">{{ $rescue->namaHewan }}</h2>
+                                <div class="d-flex align-items-center mb-3">
+                                    <p class="mb-0"><strong class="label">Jenis Hewan: </strong>{{ $rescue->jenisHewan }}</p>
+                                </div>
+                                <div class="d-flex align-items-center mb-3">
+                                    <p class="mb-0"><strong class="label">Berat Badan: </strong>{{ $rescue->bbHewan }}</p>
+                                </div>
+                                <div class="d-flex align-items-center mb-3">
+                                    <p class="mb-0"><strong class="label">Ciri Unik/Deskripsi Hewan: </strong>{{ $rescue->deskripsiHewan }}</p>
+                                </div>
+                                <div class="d-flex align-items-center mb-3">
+                                    <p class="mb-0"><strong class="label">Kondisi hewan: </strong>{{ $rescue->kondisiHewan }}</p>
+                                </div>
+                                <div class="d-flex align-items-center mb-3">
+                                    <p class="mb-0"><strong class="label">Tanggal dan Lokasi: </strong>{{ $rescue->tglLokasiPenemuan }} </p>
+                                </div>
+                                <div class="d-flex align-items-center mb-3">
+                                    <p class="mb-0"><strong class="label">Kondisi lingkungan: </strong>{{ $rescue->kondisiLingkungan }} </p>
+                                </div>
+                                <div class="d-flex align-items-center mb-3">
+                                    <p class="mb-0"><strong class="label">Tanggal Dibuatnya laporan: </strong>{{ $rescue->created_at }} </p>
+                                </div>
+                                <div class="d-flex align-items-center mb-3">
+                                    <p class="mb-0">
+                                        <strong class="label">Status: </strong>
+                                        <span class="{{ $rescue->status == 'pending' ? 'bg-danger' : ($rescue->status == 'assigned' ? 'bg-warning' : 'bg-accent') }} rounded px-2 py-1">
+                                            {{ $rescue->status }}
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+    </div>
+    </div>
+
+
+
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-beta1/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
